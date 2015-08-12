2488136bf6cfce2c51aab987daf267c1e622c42b
bret.ikehara <bret.ikehara@dailymotion.com>
2015-08-12 11:50:29 -0700

    Added commands for easier use

---
+++ README.md
@@ -0,0 +1,27 @@
+Duck DNS Manual Update Script
+-----------------------------
+
+## Quickstart
+
+    ./duckdns.sh list
+    ./duckdns.sh bind $interface
+
+### List available IPv4 interfaces
+
+Running the script will list available IPv4 interfaces:
+
+    ./duckdns.sh list
+    ./duckdns.sh -l
+
+### Bind on an interface
+
+In the terminal run:
+
+    ./duckdns.sh bind $interface
+    ./duckdns.sh -b $interface
+
+### Help
+
+Run the command by itself for help:
+
+    ./duckdns.sh
--- duckdns.sh
+++ duckdns.sh
@@ -5,7 +5,8 @@ export DOMAIN=
 export TOKEN=
 
 # script variables
-export INTERFACE=$1
+export CMD=$1
+export INTERFACE=$2
 
 function global_ip () {
 	echo `dig +short myip.opendns.com @resolver1.opendns.com`
@@ -15,47 +16,72 @@ function interface_ip () {
 	echo `ifconfig $1 | grep "inet " | grep -v 127.0.0.1 | awk '{print $2}'`
 }
 
-if [ "$INTERFACE" == "public" ]; then
-	export IP=$(global_ip)
-
-	echo "Trying to bind duckdns to public IP: ${IP}"
+function list_interfaces () {
 	echo
-elif [ "$INTERFACE" != "" ]; then
-	export IP=$(interface_ip $INTERFACE)
-
-	echo
-	echo "Trying to bind duckdns to interface $INTERFACE - $IP"
-	echo
-else
 	echo "Bind options:"
-	echo "public\t- $(global_ip)"
+	echo "\tpublic\t- $(global_ip)"
 
 	for inter in `ifconfig -l`; do
-		export IP=$(interface_ip $inter)
+		local IP=$(interface_ip $inter)
 		if [ "${IP}" != "" ]; then
-			echo "$inter\t- ${IP}"
+			echo "\t$inter\t- ${IP}"
 		fi
 	done
-
-	exit
-fi
-
-if [ "$IP" = "" ]; then
-	echo "IP Address was not found on interface $INTERFACE"
 	echo
-	exit
-fi
+}
 
-export "URL=https://www.duckdns.org/update?domains=${DOMAIN}&token=${TOKEN}&ip=${IP}"
+function show_help () {
+	echo
+	echo "Commands:"
+	echo "bind or -b $interface"
+	echo "\tBinds Duck DNS address to interface IP address\n"
+	echo "list or -l"
+	echo "\tLists available interfaces to bind\n"
+}
 
-if [ "`curl --silent $URL`" = "OK" ]; then
-	echo "Duckdns was successfully updated to ${IP}"
+function bind_ip () {
+	local IP=$1
+	local "URL=https://www.duckdns.org/update?domains=${DOMAIN}&token=${TOKEN}&ip=${IP}"
+
+	if [ "$IP" = "" ]; then
+		echo "IP Address was not found on interface $INTERFACE"
+		echo
+		return
+	fi
+
+	if [ "`curl --silent $URL`" = "OK" ]; then
+		echo "Duckdns was successfully updated to ${IP}"
+	else
+		echo "Duckdns update failed for $INTERFACE ${IP}"
+		echo
+		echo $URL
+		echo
+	fi
+}
+
+if [ "$CMD" = "bind" ] || [ "$CMD" = "-b" ]; then
+	if [ "$INTERFACE" = "" ]; then
+		echo "Please define an interface to bind"
+		echo
+		exit
+	elif [ "$INTERFACE" = "public" ]; then
+		export IP=$(global_ip)
+
+		echo "Trying to bind duckdns to public IP: ${IP}"
+		echo
+
+		bind_ip $IP
+	else
+		export IP=$(interface_ip $INTERFACE)
+
+		echo
+		echo "Trying to bind duckdns to interface $INTERFACE - $IP"
+		echo
+
+		bind_ip $IP
+	fi
+elif [ "$CMD" = "list" ] || [ "$CMD" = "-l" ]; then
+	list_interfaces
 else
-	echo "Duckdns update failed for $INTERFACE ${IP}"
-	echo
-	echo $URL
-	echo
+	show_help
 fi
-
-echo
-echo
